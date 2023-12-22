926eb89438fc51b113317b3f645ebe248aaa18c2
peterbyao <42585479+peterbyao@users.noreply.github.com>
2023-12-22 09:23:56 -0500

    Add files via upload

---
+++ app/src/aws.py
@@ -0,0 +1,13 @@
+
+class env():
+    aws_user = "peterbyao_6156"
+    aws_console_pw = ""
+    aws_user_access_key = ""
+    aws_secret_access_key = ""
+    arn = "arn:aws:sns:us-east-2:037809521127:NewMember"
+
+class rds():
+    user = "admin"
+    password = "TradingApp1234"
+    endpoint = "mysql-instanceee4a4a3.cvibjvlupbqt.us-east-2.rds.amazonaws.com"
+    schema = "pulumimembers"
